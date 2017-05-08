eeb351c5ff9e908542edd79504d51ae55fc5b14f
Kevin Madison <kelevra@atlantis.localdomain>
2017-05-08 04:42:57 +0000

    startup

--- startup.sh
+++ startup.sh
@@ -5,3 +5,5 @@ sudo apt-get install npm
 sudo npm install n -g
 sudo n stable
 sudo npm install -g @angular/cli@latest
+
+"""The Go app should run fine, the angular part you may need to edit .angular-cli.json down at the bottom in the defaults sections. Also you will have to change the websocket in the constructor of src/app/socket.service.ts."""
