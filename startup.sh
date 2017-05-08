ca9122153cb1f6b4536ae903676e79a5b4876428
Kevin Madison <kelevra@atlantis.localdomain>
2017-05-08 03:12:28 +0000

    fixing

---
+++ startup.sh
@@ -0,0 +1,7 @@
+go get github.com/gorilla/websocket
+go get github.com/satori/go.uuid
+sudo apt-get install nodejs
+sudo apt-get install npm
+sudo npm install n -g
+sudo n stable
+sudo npm install -g @angular/cli@latest
